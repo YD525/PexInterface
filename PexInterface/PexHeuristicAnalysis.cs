2985d8ec9a1638250a17152ba3acfee3a324a2a0
YD525 <525084464@qq.com>
2026-03-01 10:58:46 +0800

    Implement a mechanism that determines whether a variable can be translated based on the method name and the position of the parameters passed to it.

--- PexInterface/PexHeuristicAnalysis.cs
+++ PexInterface/PexHeuristicAnalysis.cs
@@ -11,6 +11,57 @@ namespace PexInterface
     {
         public static string Version = "1.0.0";
 
+        //https://ck.uesp.net/wiki/Category:Papyrus Game Api Doc
+        //public static List<string> DangerPapyrusFuncs = new List<string>
+        //{
+        //    "DamageActorValue",//Game Api https://ck.uesp.net/wiki/DamageActorValue
+        //    "AttrDrain",//Game Api ?? maybe SkSE
+        //    "RandomExpressionByTag",//SexLab Api
+        //    "SendDeviceEvent",//DD Api
+        //    "SendModEvent",//Game Api https://ck.uesp.net/wiki/SendModEvent
+        //    "UnSetFormValue",//storageutil Api
+        //    "AnimSwitchKeyword",//Game Api ?? maybe SkSE 
+        //    "StartThirdPersonAnimation",//Game Api ?? maybe SkSE 
+        //    "SendAnimationEvent",//Game Api https://ck.uesp.net/wiki/SendAnimationEvent_-_Debug
+        //    "SetInstanceVolume",//Game Sound Api https://ck.uesp.net/wiki/SetInstanceVolume_-_Sound
+        //    "Create",//Game ModEvent Api https://ck.uesp.net/wiki/ModEvent_Script
+        //};
+
+        //public static List<string> UserDefinedSafeFuncs = new List<string>() {
+        //"Notify", "notify",
+        //"Log", "log",
+        //"Msg", "msg", "MSG",
+        //"Message", "message",
+        //"ShowMessage", "showMessage", "ShowMsg", "showMsg",
+        //"Display", "display",
+        //"Print", "print",
+        //"Alert", "alert",
+        //"Popup", "popup", "PopUp", "popUp",
+        //"Toast", "toast",
+        //"DebugLog", "debugLog", "Debug", "debug",
+        //"WriteLine", "writeline", "Write", "write",
+        //"ShowNotification", "showNotification", "Notification", "notification",
+        //"ShowInfo", "showInfo", "Info", "info",
+        //"ShowText", "showText",
+        //"ConsoleLog", "consoleLog",
+        //};
+
+        //public static List<FuncCheck> MethodSafeParams = new List<FuncCheck>()
+        //{
+        //   new FuncCheck("NotifyPlayer",0),//DD Api
+        //   new FuncCheck("NotifyNPC",0),//DD Api
+        //   new FuncCheck("AddSliderOption",0),//SkyUI Api
+        //   new FuncCheck("AddSliderOptionST",1),//SkyUI Api
+        //   new FuncCheck("AddTextOption",0),//SkyUI Api
+        //   new FuncCheck("AddTextOptionST",1),//SkyUI Api
+        //   new FuncCheck("AddToggleOption",0),//SkyUI Api
+        //   new FuncCheck("AddToggleOptionST",1),//SkyUI Api
+        //   new FuncCheck("SetGameSettingString",1),//?? Api
+        //   new FuncCheck("ShowMessage",0)//DDI Api
+        //};
+
+        public FuncRule FuncNameCheck = null;
+
         public List<PexStringItem> Strings = new List<PexStringItem>();
         public PscCls CurrentCls = new PscCls();
 
@@ -28,6 +79,22 @@ namespace PexInterface
         {
             Strings.Clear();
             CurrentCls = SetClass;
+            Init();
+        }
+
+        public void Init()
+        {
+            FuncNameCheck = new FuncRule();
+            FuncNameCheck.AddFuncRule("NotifyPlayer",ScoreEvaluator.Safe,0);
+            FuncNameCheck.AddFuncRule("NotifyNPC", ScoreEvaluator.Safe, 0);
+            FuncNameCheck.AddFuncRule("AddSliderOption", ScoreEvaluator.Safe, 0);
+            FuncNameCheck.AddFuncRule("AddSliderOptionST", ScoreEvaluator.Safe, 1);
+            FuncNameCheck.AddFuncRule("AddTextOption", ScoreEvaluator.Safe, 0);
+            FuncNameCheck.AddFuncRule("AddTextOptionST", ScoreEvaluator.Safe, 1);
+            FuncNameCheck.AddFuncRule("AddToggleOption", ScoreEvaluator.Safe, 0);
+            FuncNameCheck.AddFuncRule("AddToggleOptionST", ScoreEvaluator.Safe, 1);
+            FuncNameCheck.AddFuncRule("SetGameSettingString", ScoreEvaluator.Safe, 1);
+            FuncNameCheck.AddFuncRule("ShowMessage", ScoreEvaluator.Safe, 0);
         }
         public string GetPsc(CodeGenStyle Style = CodeGenStyle.CSharp)
         {
@@ -244,6 +311,78 @@ namespace PexInterface
             public string Original = "";
             public string Translated = "";
         }
+
+
+        public class ScoreEvaluator
+        {
+            public static int Null = 0;
+            public static int Safe = 100;
+            public static int Normal = 60;
+            public static int PotentialRisk = 1;
+            public static int HighRisk = -1;
+        }
+
+        public class ParamCheck
+        {
+            public int Index = 0;
+            public bool FullCheck = false;
+            public int Score = 0;
+        }
+     
+        public class FuncRule
+        {
+            public Dictionary<string,List<ParamCheck>> Checks = new Dictionary<string, List<ParamCheck>>();
+            public void AddFuncRule(string FuncName,int Score, int ParamIndex = -1)
+            {
+                ParamCheck NParamCheck = new ParamCheck();
+
+                if (ParamIndex >= 0)
+                {
+                    NParamCheck.Index = ParamIndex;
+                }
+                else
+                {
+                    NParamCheck.FullCheck = true;
+                }
+
+                NParamCheck.Score = Score;
+                
+                if (Checks.ContainsKey(FuncName))
+                {
+                    Checks[FuncName].Add(NParamCheck);
+                }
+                else
+                {
+                    Checks.Add(FuncName,new List<ParamCheck>() { NParamCheck } );
+                }
+            }
+            public int CheckFuncByName(string FuncName,int ParamIndex = 0)
+            {
+                if (Checks.ContainsKey(FuncName))
+                {
+                    if (Checks[FuncName].Count > 0)
+                    {
+                        if (Checks[FuncName][0].FullCheck)
+                        {
+                            return Checks[FuncName][0].Score;
+                        }
+                        else
+                        {
+                            foreach (var GetCheck in Checks[FuncName])
+                            {
+                                if (GetCheck.Index.Equals(ParamIndex))
+                                {
+                                    return GetCheck.Score;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                return ScoreEvaluator.Null;
+            }
+        }
+
     }
 
 
